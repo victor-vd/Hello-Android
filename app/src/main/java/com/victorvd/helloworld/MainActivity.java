afc016fc1e5ba4d0f8c717d936b0afee2c370f92
ohtomu <150762200+ohtomu@users.noreply.github.com>
2025-03-12 03:41:30 -0300

    visual features explored

--- app/src/main/java/com/victorvd/helloworld/MainActivity.java
+++ app/src/main/java/com/victorvd/helloworld/MainActivity.java
@@ -1,6 +1,13 @@
 package com.victorvd.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.victorvd.helloworld.R;
+
+import java.util.Locale;
+import java.util.Random;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Random rnd = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+    public void rollDice(View view){
+
+        TextView textDiceResult = findViewById(R.id.textDiceResult);
+
+        int rolledNumber  = (int) Math.ceil(rnd.nextDouble()*20);
+
+        textDiceResult.setText(String.format(Locale.getDefault(), "Your number is %d", rolledNumber));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
